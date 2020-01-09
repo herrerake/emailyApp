e7519d67dc0dd065d50fbec4a5d39418fba22cb7
Kevin Herrera <kherrera@Kevins-Air.attlocal.net>
2020-01-09 15:38:11 -0600

    Added billing and client side app! Heroku build process added

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
 
@@ -13,7 +14,13 @@ class Header extends Component {
                     <li><a href="/auth/google">Login With Google</a></li>
                 )
             default:
-                return <li><a href="/api/logout">Logout</a></li>
+                return (
+                    <Fragment>
+                        <li><Payments /></li>
+                        <li style={{ margin: '0 10px'}}>Credits: {this.props.auth.credits}</li>
+                        <li><a href="/api/logout">Logout</a></li>
+                    </Fragment>
+                );
         }
     }
     render() {
